eeb65511033a670b2b423ee580de8119b6f04bcc
carterellis <carterellis05@gmail.com>
2024-12-09 22:19:13 -0500

    Using git from bash command line

---
+++ test.py
@@ -0,0 +1,11 @@
+from waitress import serve
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+serve(app, port=5000)
