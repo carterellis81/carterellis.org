5d22f9659532dd07fdfd9437ff8ca41ea598b0fa
carterellis <carterellis05@gmail.com>
2024-12-12 01:11:38 -0500

    i love git bash actually

--- main.py
+++ main.py
@@ -3,13 +3,4 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
-@app.route('/')
-def landing_page():
-    return render_template('index.html')
-
-@app.route('/christmas')
-def wish_list():
-    return render_template('wishlist.html')
-
-serve(app, port=5000)
+gt
