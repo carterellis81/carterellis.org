594070fb46985c40c462553eded05e0586de4006
carterellis <carterellis05@gmail.com>
2024-12-12 01:13:26 -0500

    Revert "i love git bash actually"
    This reverts commit 5d22f9659532dd07fdfd9437ff8ca41ea598b0fa.

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-gt
+
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
+@app.route('/christmas')
+def wish_list():
+    return render_template('wishlist.html')
+
+serve(app, port=5000)
