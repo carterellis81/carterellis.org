5ff618f308fa1050891e739260d3bafeffd5bbeb
carterellis <carterellis05@gmail.com>
2024-12-13 15:06:32 -0500

    splash text on main page

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,18 +1,28 @@
-var wrapper = document.getElementById("HEADWRAPPER");
-var optionsButton = document.getElementById("OPTIONBUTTON");
+const title = document.getElementById("TITLE");
 
-optionsButton.onclick = toggleVisibility;
-wrapper.style.visibility = 'hidden';
-optionsButton.style.visibility = 'visible';
+const textSplashes = [
+    "innovating the future",
+    "investing in schizocoin",
+    "currently not studying",
+    "yes, that's my name",
+    "willing to work for money",
+    "donations accepted",
+    "now non-GMO!",
+    "now with extra GMOs!"
+];
 
-function toggleVisibility() {
-    if (wrapper.style.visibility === 'hidden') {
-        wrapper.style.visibility = 'visible';
-        optionsButton.innerHTML = 'hide links';
+window.addEventListener('load', (event) => {
+    let num = getRandomInt(0, Math.ceil(textSplashes.length * 1.3));
+    if (num <= (textSplashes.length - 1)) {
+        title.innerHTML = "<h1> carter ellis: <span style='color: red;'>" + textSplashes[num] + "</span> </h1>";
     }
     else {
-        wrapper.style.visibility = 'hidden';
-        optionsButton.style.visibility = 'visible';
-        optionsButton.innerHTML = 'show links';
+        title.innerHTML = "<h1> carter ellis </h1>";
     }
+})
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
