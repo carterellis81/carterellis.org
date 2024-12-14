b68d2938321ed8dda88479ad50412f8b61940cc7
carterellis <carterellis05@gmail.com>
2024-12-14 01:37:23 -0500

    update colors

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -11,15 +11,21 @@ const textSplashes = [
     "now with extra GMOs!"
 ];
 
-window.addEventListener('load', (event) => {
-    let num = getRandomInt(0, Math.ceil(textSplashes.length * 1.3));
-    if (num <= (textSplashes.length - 1)) {
+window.addEventListener('load', changeTextSplash);
+title.addEventListener('click', changeTextSplash)
+
+function changeTextSplash() {
+    let num = getRandomInt(0, Math.ceil(textSplashes.length + 3));
+    if ((num <= textSplashes.length - 1) && !(title.innerHTML.includes(textSplashes[num]))) {
         title.innerHTML = "<h1> carter ellis: <span style='color: rgb(168, 25, 25);'>" + textSplashes[num] + "</span> </h1>";
+    }
+    else if ((num <= textSplashes.length - 1)) {
+        changeTextSplash();
     }
     else {
         title.innerHTML = "<h1> carter ellis </h1>";
     }
-})
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
