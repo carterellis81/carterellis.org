507553ca3bb99be9114772207df95c9c4f750e66
carterellis <carterellis05@gmail.com>
2024-12-15 17:48:54 -0500

    fixed header button destination

--- static/scripts/canvas.js
+++ static/scripts/canvas.js
@@ -64,7 +64,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 100) {
+while (balls.length < 250) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
